948ef4d0a63cf9284075402795192c4f7be9b769
“Iryna <ihryshanovich@mediaocean.com>
2025-05-02 01:50:10 -0700

    updated start-backend.sh

--- start-backend.sh
+++ start-backend.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 cd ~/automation-suite/backend
-python3 -m uvicorn app.main:app --host 0.0.0.0 --port 8000
+
+if [ ! -d "venv" ]; then
+    python3 -m venv venv
+    source venv/bin/activate
+    pip install -r requirements.txt
+else
+    source venv/bin/activate
+fi
+
+# Start the FastAPI application
+python -m uvicorn app.main:app --host 0.0.0.0 --port 8000
