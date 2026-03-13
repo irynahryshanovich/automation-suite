e9254411500571d9d084e4e3ddbcee8ea532c900
“Iryna <ihryshanovich@mediaocean.com>
2026-03-13 12:10:37 -0700

    remove scheduler.shutdown from init_scheduler since it implemented in lifespan imn main

--- backend/app/scheduler.py
+++ backend/app/scheduler.py
@@ -2,8 +2,6 @@ from apscheduler.schedulers.background import BackgroundScheduler
 from apscheduler.triggers.interval import IntervalTrigger
 from apscheduler.jobstores.base import JobLookupError
 from datetime import datetime
-import atexit
-
 from app.services.automation_service import perform_automation
 from app.config import settings
 
@@ -29,9 +27,6 @@ def init_scheduler():
         # Start the scheduler
         scheduler.start()
         print(f"Scheduler started. Running every {settings.AUTOMATION_CADENCE} minutes")
-
-        # Shut down the scheduler when exiting the app
-        atexit.register(lambda: scheduler.shutdown())
     except Exception as e:
         print(f"Error initializing scheduler: {e}")
 
